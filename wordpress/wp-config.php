b0ad674ea83f29847fe427cac6185b666dbc3a7c
Your Name <you@example.com>
2023-09-10 11:35:13 -0300

    first commit

---
+++ wordpress/wp-config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', 'db');
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', '');
+
